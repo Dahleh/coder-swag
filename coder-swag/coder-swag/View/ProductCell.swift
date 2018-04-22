37f3bd73cd4dd9388ece8a2e19d0bdb84518014b
Faris Dahleh <farisdahleh@Live.com>
2018-04-23 02:39:41 +0300

    collectionView

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Faris Dahleh on 4/23/18.
+//  Copyright © 2018 Faris Dahleh. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
